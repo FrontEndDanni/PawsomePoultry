b52318ab3da4ad6e2d8004f6cacb3a7500c6ecf3
FrontEndDanni <dannikabdull@gmail.com>
2023-06-29 01:24:49 -0700

    added placeholder links to footer

--- src/App.js
+++ src/App.js
@@ -30,7 +30,10 @@ function App() {
       </div>
       </div>
       <div className="footer">
-        &copy; 2023 Pawsome Poultry. All rights reserved.
+        &copy; 2023 Pawsome Poultry. All rights reserved.<br></br>
+        <a href="about-us.html">About Us</a> | 
+        <a href="contact-us.html">Contact Us</a> | 
+        <a href="shipping-returns.html">Shipping & Returns</a>
       </div>
     </div>
   );
