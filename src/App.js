2a43cd2e9c9fb353a721be8c87ab105405fd55ba
FrontEndDanni <dannikabdull@gmail.com>
2023-06-29 01:16:58 -0700

    made navbar sticky, added images

--- src/App.js
+++ src/App.js
@@ -4,13 +4,30 @@ function App() {
   return (
     <div className="app">
       <Navbar />
+      <br></br>
       <div className="header">
         <h1>Pawsome Poultry</h1>
       </div>
       <div className="content">
         <p>At Pawsome Poultry, we offer premium chicken jerky treats for your beloved dogs. Made with high-quality ingredients and packed with flavor, our jerky treats are a healthy and delicious choice for your furry friends.</p>
         <p>Shop now and give your dogs a tasty and nutritious treat they'll love!</p>
+        <div className="image-container">
+        <div className="image-wrapper">
+          <img src="https://picsum.photos/200/200" alt="Placeholder Image" className="placeholder-image" />
+          <p className="image-caption">100% natural!</p>
+        </div>
+        <div className="image-wrapper">
+          <img src="https://picsum.photos/200/200" alt="Placeholder Image" className="placeholder-image" />
+          <p className="image-caption">100% American-made!</p>
+        </div>
+        <div className="image-wrapper">
+          <img src="https://picsum.photos/200/200" alt="Placeholder Image" className="placeholder-image" />
+          <p className="image-caption">100% tasty!</p>
+        </div>
+      </div>
+      <div className="cta">
         <a href="#" className="cta-button">Shop Now</a>
+      </div>
       </div>
       <div className="footer">
         &copy; 2023 Pawsome Poultry. All rights reserved.
